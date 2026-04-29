c42a3f9b4da4081726375cda6b21cec8f2f3caa3
markus <markus@brunokrams.de>
2026-04-29 08:46:56 +0200

    Works but is still to slow

---
+++ test_data_generation/datafiles_generator.py
@@ -0,0 +1,26 @@
+import logging
+import os
+
+from json_generator import JsonGenerator
+from wav_generator import WavGenerator
+
+class DataGenerator():
+
+    logger = logging.getLogger("DataGenerator")
+
+    def __init__(self, target_dir: str):
+        self.target_dir = target_dir
+
+    def generate(self, move):
+        identifier =  move.identifier()
+        self.logger.info(f"Generating files for move {identifier}")
+        os.makedirs(self.target_dir, exist_ok=True)
+        move_dir = os.path.join(self.target_dir, identifier)
+        os.makedirs(move_dir, exist_ok=False)
+        JsonGenerator(move_dir).generate(move)
+        wav_gen = WavGenerator(base_dir=move_dir)
+        for i, text in enumerate(move.full_texts()):
+            wav_gen.generate(text, f"text_{i}")
+        self.logger.info(f"Finished generating files for move {identifier}")
+
+
